3525920117fd788d83e59d1cd7b5e5ffab3561fa
macdoum1 <macdoum1@tcnj.edu>
2014-04-08 21:27:08 -0400

    EtsySearch sends asynchronous using completion block rather than NSURLConnection delegate methods

--- Etsy InfiniSearch/Etsy InfiniSearch/EtsySearch.m
+++ Etsy InfiniSearch/Etsy InfiniSearch/EtsySearch.m
@@ -9,9 +9,6 @@
 #import "EtsySearch.h"
 
 @implementation EtsySearch
-{
-    NSMutableData *responseData;
-}
 
 - (id) init
 {
@@ -25,9 +22,6 @@
 
 - (void) searchWithKeyword:(NSString *)keyword andSortMethod: (EtsySortMethod *)sortMethod
 {
-    // Initialize Response Data
-    responseData = [[NSMutableData alloc]init];
-    
     // Create NSString using API URL, API Key, and the contents of the search bar
     NSString *urlString = [NSString stringWithFormat:@"https://api.etsy.com/v2/listings/active?api_key=%@&includes=MainImage&keywords=%@&offset=%d%@&limit=%d",API_KEY,keyword,self.currentOffset,sortMethod.sortPrefix,NUM_RESULTS_PER_LOAD];
     
@@ -37,74 +31,55 @@
     // Create NSURLRequest object from URL
     NSURLRequest *request = [[NSURLRequest alloc]initWithURL:requestURL];
     
-    // Create NSURLConnection from request object
-    NSURLConnection *connection = [[NSURLConnection alloc]initWithRequest:request delegate:self];
-    if(!connection)
-    {
-        [_delegate searchFailedWithError:nil];
-    }
-    _currentOffset = _currentOffset + NUM_RESULTS_PER_LOAD;
-}
-
-// NSURLConnection didReceiveResponse Method
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
-{
-    [responseData setLength:0];
-}
-
-// NSURLConnection didReceiveData Method
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
-{
-    [responseData appendData:data];
-}
-
-// NSURLConnection didFailWithError Method
-- (void)connection:(NSURLConnection *)connection
-  didFailWithError:(NSError *)error
-{
-    // Alert delegate that search failed
-    [_delegate searchFailedWithError:error];
-}
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection
-{
-    NSError *error;
-    
-    // Parse response data to dictionary object using JSONSerialization
-    NSDictionary *responseDataDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
-    
-    if(error == NULL)
-    {
-        // Alert delegate that search is finished and pass results
-        if([[responseDataDictionary objectForKey:@"count"] intValue] > 0)
+    // Asynchronous NSURLConnection
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError){
+        NSError *error;
+        
+        if(connectionError == NULL)
         {
-            // Initialize array for list of Etsy Listing Objects
-            NSMutableArray *parsedEtsyListings = [[NSMutableArray alloc]init];
+            // Parse response data to dictionary object using JSONSerialization
+            NSDictionary *responseDataDictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
             
-            // Get results from dictionary
-            NSArray* allResults = [responseDataDictionary objectForKey:@"results"];
-            
-            // Parse results into EtsyListing objects
-            for(NSDictionary *currentResult in allResults)
+            if(error == NULL)
             {
-                // Initialize object with title and imageURL from dictionary
-                EtsyListing *currentListing = [[EtsyListing alloc]
-                                               initWithTitle:[[currentResult objectForKey:@"title"] kv_decodeHTMLCharacterEntities]
-                                               andListingImageURL:[[currentResult objectForKey:@"MainImage"] objectForKey:@"url_170x135"]];
-                // Add to array
-                [parsedEtsyListings addObject:currentListing];
+                // Alert delegate that search is finished and pass results
+                if([[responseDataDictionary objectForKey:@"count"] intValue] > 0)
+                {
+                    // Initialize array for list of Etsy Listing Objects
+                    NSMutableArray *parsedEtsyListings = [[NSMutableArray alloc]init];
+                    
+                    // Get results from dictionary
+                    NSArray* allResults = [responseDataDictionary objectForKey:@"results"];
+                    
+                    // Parse results into EtsyListing objects
+                    for(NSDictionary *currentResult in allResults)
+                    {
+                        // Initialize object with title and imageURL from dictionary
+                        EtsyListing *currentListing = [[EtsyListing alloc]
+                                                       initWithTitle:[[currentResult objectForKey:@"title"] kv_decodeHTMLCharacterEntities]
+                                                       andListingImageURL:[[currentResult objectForKey:@"MainImage"] objectForKey:@"url_170x135"]];
+                        // Add to array
+                        [parsedEtsyListings addObject:currentListing];
+                    }
+                    
+                    [_delegate searchDidFinish:parsedEtsyListings];
+                }
+                else
+                {
+                    [_delegate noResultsFound];
+                }
             }
-
-            [_delegate searchDidFinish:parsedEtsyListings];
+            else
+            {
+                [_delegate searchFailedWithError:error];
+            }
         }
         else
         {
-            [_delegate noResultsFound];
+            [_delegate searchFailedWithError:connectionError];
         }
-    }
-    else
-    {
-        [_delegate searchFailedWithError:error];
-    }
+    }];
+    _currentOffset = _currentOffset + NUM_RESULTS_PER_LOAD;
 }
 
 @end
